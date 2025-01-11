e152c40d7afcbb1aeb475232b2182317d0a6c884
kevinxu <kevin671xu@gmail.com>
2025-01-11 17:42:00 +0900

    add Countdown

---
+++ src/dataset/countdown.py
@@ -0,0 +1,63 @@
+import torch
+from torch.utils.data import Dataset
+import json
+
+OP_TO_CODE = {
+    '+': 1,
+    '-': 2,
+    '*': 3,
+    '/': 4,
+    '=': 5
+}
+OFFSET = len(OP_TO_CODE)
+
+class CountDownDataset(Dataset):
+    def __init__(self, json_file_path):
+        with open(json_file_path, 'r') as f:
+            self.data = json.load(f)
+
+
+    def __len__(self):
+        return len(self.data)
+
+    def parse_expression(self, expression):
+        parts = []
+        num = ""
+        for char in expression:
+            if char.isdigit():
+                num += char  # 数字を連結
+            else:
+                if num:
+                    parts.append(int(num) + OFFSET)  # 数値にOFFSETを加算してリストに追加
+                    num = ""
+                parts.append(OP_TO_CODE[char])  # 演算子をコードに変換して追加
+        if num:
+            parts.append(int(num) + OFFSET)  # 最後の数値を追加
+
+        return parts
+
+    def __getitem__(self, idx):
+        sample = self.data[idx]
+        nums = [num + OFFSET for num in sample['nums']]  # numsにOFFSETを加算
+        target = sample['target'] + OFFSET  # ターゲットにOFFSETを加算
+
+        solution_steps = []
+        for step in sample['solution']:
+            solution_steps.extend(self.parse_expression(step))  # すべての手順をリストに展開
+
+        # テンソル形式に変換
+        input_tensor = torch.tensor(nums + [target], dtype=torch.long)
+        output_tensor = torch.tensor(solution_steps, dtype=torch.long)
+
+        return input_tensor, output_tensor
+    
+
+# 使用例
+json_file_path = "/work/gg45/g45004/tf-backtrack/data/b4_3_random/train1_b4_t100_n10_random.json"  # JSONファイルパス
+dataset = CountDownDataset(json_file_path)
+
+# 動作確認
+for i in range(len(dataset)):
+    x, y = dataset[i]
+    print(x.shape, y.shape) # 
+    print(f"Input (nums): {x.numpy()}, Output (solution steps): {y.numpy()}")
---
+++ src/dataset/sos/gen_task.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python countdown_generate.py --seed 4 --data_dir /work/gg45/g45004/tf-backtrack/data/b4_3_random/ --min_range 4 --start_range 4 --num_samples 500000
